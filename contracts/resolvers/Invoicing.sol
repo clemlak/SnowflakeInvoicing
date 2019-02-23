1a7baddfce1ba9d769ca77a44d59defaf78442b3
Clement Lakhal <clementlakhal@gmail.com>
2019-02-23 19:37:14 +0100

    Added comments

--- contracts/resolvers/Invoicing.sol
+++ contracts/resolvers/Invoicing.sol
@@ -1,4 +1,6 @@
-pragma solidity ^0.5.0;
+/* solhint-disable no-empty-blocks */
+
+pragma solidity 0.5.0;
 
 import "../SnowflakeResolver.sol";
 import "../interfaces/IdentityRegistryInterface.sol";
@@ -6,10 +8,19 @@ import "../interfaces/HydroInterface.sol";
 import "../interfaces/SnowflakeInterface.sol";
 
 
+/**
+ * @title Invoicing
+ * @notice Create invoices through Snowflake
+ * @dev This contract is the base of the dapp
+ */
 contract Invoicing is SnowflakeResolver {
+    /* Invoices can have several status, but only one at a time */
     enum Status { Draft, Unpaid, PartiallyPaid, Paid, PartiallyRefunded, Refunded, Canceled, Disputed }
+
+    /* These are our payment terms */
     enum Terms { DueOnReceipt, DueOnDate, NoDueDate, NetD }
 
+    /* This defines the invoices */
     struct Invoice {
         Status status;
         uint256 date;
@@ -28,37 +39,58 @@ contract Invoicing is SnowflakeResolver {
 
     Invoice[] private invoices;
 
+    /* We link the merchants to their invoices */
     mapping (uint256 => uint256[]) public merchantsToInvoices;
+
+    /* We link the customers to their invoices */
     mapping (uint256 => uint256[]) public customersToInvoices;
 
-    /**
-     * @dev We keep track of the payments here
-     * The struct is invoiceId => ein => amount
-     */
+    /* We keep track of the payments here (invoiceId => ein => amount) */
     mapping (uint256 => mapping (uint256 => uint256)) public payments;
 
-    /**
-     * @dev We keep track of the refunds here
-     * The struct is invoiceId => ein => amount
-     */
+    /* We keep track of the refunds here (invoiceId => ein => amount) */
     mapping (uint256 => mapping (uint256 => uint256)) public refunds;
 
-    event LogInvoiceCreated(uint256 invoiceId, address indexed merchant, address[] customers);
+    /**
+     * @dev Emitted when an invoice is created
+     * @param invoiceId The id of the new invoice
+     * @param merchant The ein of the merchant
+     * @param customers the ein of the customers
+     */
+    event LogInvoiceCreated(uint256 invoiceId, uint256 indexed merchant, uint256[] customers);
 
-    event LogPayment(uint256 invoiceId, address customer, uint256 amount);
+    /**
+     * @dev Emitted when a payment is made
+     * @param invoiceId The id of the new invoice
+     * @param customer The ein of the customer
+     * @param amount The paid amount
+     */
+    event LogPayment(uint256 invoiceId, uint256 customer, uint256 amount);
 
-    event LogRefund(uint256 invoiceId, address customer, uint256 amount);
+    /**
+     * @dev Emitted when a refund is made
+     * @param invoiceId The id of the new invoice
+     * @param customer The ein of the customer
+     * @param amount The refunded amount
+     */
+    event LogRefund(uint256 invoiceId, uint256 customer, uint256 amount);
 
-    constructor (address snowflakeAddress)
-        SnowflakeResolver("Invoicing", "Create invoices", snowflakeAddress, false, false) public
-    {}
+    constructor (address snowflakeAddress) public
+        SnowflakeResolver("Invoicing", "Create invoices", snowflakeAddress, false, false) {}
 
-    // implement signup function
-    function onAddition(uint ein, uint, bytes memory) public senderIsSnowflake() returns (bool) {
-    }
+    function onAddition(uint ein, uint, bytes memory) public senderIsSnowflake() returns (bool) {}
 
     function onRemoval(uint, bytes memory) public senderIsSnowflake() returns (bool) {}
 
+    /**
+     * @dev Creates a new draft invoice
+     * @param customers The ein of the customers
+     * @param amount The amount of the invoice
+     * @param allowPartialPayment If partial payment is allowed
+     * @param minimumAmountDue The minimum amount for a payment
+     * @param paymentTerm The type of term of payment
+     * @param term The term of payment, in days or a timestamp
+     */
     function createDraftInvoice(
         uint256[] memory customers,
         uint256 amount,
@@ -92,8 +124,15 @@ contract Invoicing is SnowflakeResolver {
         ) - 1;
 
         merchantsToInvoices[ein].push(invoiceId);
+
+        emit LogInvoiceCreated(0, ein, customers);
     }
 
+    /**
+     * @dev Updates the customers of an invoice
+     * @param invoiceId The id of the invoice
+     * @param customers The updated customers list
+     */
     function updateInvoiceCustomers(
         uint256 invoiceId,
         uint256[] memory customers
@@ -106,6 +145,17 @@ contract Invoicing is SnowflakeResolver {
         invoices[invoiceId].customers = customers;
     }
 
+    /**
+     * @dev Updates the payment data of an invoice
+     * @param invoiceId The id of the invoice
+     * @param amount The amount of the invoice
+     * @param allowPartialPayment If partial payment is allowed
+     * @param minimumAmountDue The minimum amount for a payment
+     * @param paymentTerm The type of term of payment
+     * @param term The term of payment, in days or a timestamp
+     * @param additionalTerms Additional terms (as a string)
+     * @param note Additional note (as a string)
+     */
     function updateInvoicePayment(
         uint256 invoiceId,
         uint256 amount,
@@ -130,6 +180,11 @@ contract Invoicing is SnowflakeResolver {
         invoices[invoiceId].note = note;
     }
 
+    /**
+     * @dev Gets invoice info
+     * @param invoiceId The id of the invoice
+     * @return The status, date, merchant and customers of an invoice
+     */
     function getInvoiceInfo(uint256 invoiceId) public view returns (
         Status status,
         uint256 date,
@@ -144,6 +199,11 @@ contract Invoicing is SnowflakeResolver {
         );
     }
 
+    /**
+     * @dev Gets invoice details
+     * @param invoiceId The id of the invoice
+     * @return The amount, paidAmount, refundedAmount, allowPartialPayment, paymentTerm and term of the invoice
+     */
     function getInvoiceDetails(uint256 invoiceId) public view returns (
         uint256 amount,
         uint256 paidAmount,
@@ -164,6 +224,11 @@ contract Invoicing is SnowflakeResolver {
         );
     }
 
+    /**
+     * @dev Gets invoice additional details
+     * @param invoiceId The id of the invoice
+     * @return The additionalTerms and note of the invoice
+     */
     function getInvoicesAdditionalDetails(uint256 invoiceId) public view returns (
         string memory additionalTerms,
         string memory note
@@ -174,10 +239,20 @@ contract Invoicing is SnowflakeResolver {
         );
     }
 
+    /**
+     * @dev Returns the invoices of a merchant
+     * @param ein The ein of the merechant
+     * @return The id of the invoices
+     */
     function getInvoicesFromMerchant(uint256 ein) public view returns (uint256[] memory) {
         return merchantsToInvoices[ein];
     }
 
+    /**
+     * @dev Returns the invoices of a customer
+     * @param ein The ein of the customer
+     * @return The id of the invoices
+     */
     function getInvoicesFromCustomer(uint256 ein) public view returns (uint256[] memory) {
         return customersToInvoices[ein];
     }
